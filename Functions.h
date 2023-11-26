5af7e47079ba83a380d1f05e5ed72bb31e6d6d4d
stuxbet <123492671+stuxbet@users.noreply.github.com>
2023-11-26 11:00:08 -0600

    Update Functions.h
    updated .h

--- Functions.h
+++ Functions.h
@@ -1,2 +1,11 @@
 #pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 bool mainMenu();
+void addTaskToList(string DOW, string title, string description, string ST, string ET);
+void deleteTask(string title);
+void printItenerary();
+void Help();
