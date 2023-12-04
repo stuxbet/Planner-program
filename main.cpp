495ef79ff0850f05616e43a47894ec14353fd3bd
michaelfamurewa <143642161+michaelfamurewa@users.noreply.github.com>
2023-12-04 11:41:16 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,6 +8,16 @@ int main() {
     Linked monday,tuesday, wednesday, thursday, friday, saturday, sunday;
     vector<Linked> days = {monday,tuesday, wednesday, thursday, friday, saturday, sunday};
     Task temp, temp2, temp3;
+//    temp.setTaskName("homework");
+//    temp2.setTaskName("review");
+//    temp3.setTaskName("study");
+//    temp2.setDueDate(12,24,2023);
+//    monday.addNode(temp);
+//    monday.addNode(temp2);
+//    monday.addNode(temp3);
+//    monday.printList(1);
+//    monday.delNode("Reviefhhggh");
+//    monday.printList(1);
 
     cout << "\n" << endl;
     while(mainMenu(days));
